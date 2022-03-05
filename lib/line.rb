9c0ededbfa1c745667e2a5eb18f99b580ade25be
Joseph Senecal <jasenecal2@gmail.com>
2022-03-05 12:06:10 -0700

    Feat: Line #translate

--- lib/line.rb
+++ lib/line.rb
@@ -1,10 +1,23 @@
-# reserved for future requires
+require './rosetta'
 class Line
   attr_reader :translated
 
   def initialize (input)
-    @intput = input
+    @input = input
     @translated = ""
+    @rosetta = Rosetta.from_nightwriter #refactor for nightreader
   end
-  
+
+  def translate
+    translated_input = ["","",""]
+    @input.each_char do |char|
+      translated_char = @rosetta.translate(char)
+      translated_input[0] += translated_char[2] + translated_char[3]
+      translated_input[1] += translated_char[1] + translated_char[4]
+      translated_input[2] += translated_char[0] + translated_char[5]
+    end
+    translated_input.map! {|line| line += "\n"}
+    translated_input[0] + translated_input[1] + translated_input[2]
+  end
+
 end
