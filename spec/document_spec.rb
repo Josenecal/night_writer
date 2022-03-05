1519ec1749edf744c5bf2e7b4e7a83ddfbeb11bb
Joseph Senecal <jasenecal2@gmail.com>
2022-03-05 15:13:39 -0700

    add #parse test

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -26,8 +26,10 @@ RSpec.describe Document do
     expect(expected).to eq(["the quick red fox jumped over the lazy", "brown dog and the lazy brown dog chased", "that fox all the way back to his den"])
   end
 
-  xit "parses paragraphs into lines of 40 characters or less" do
-    expect(@document.parse).to eq(["The quick red fox jumped over the lazy", "brown dog"])
+  it "breaks down several paragraphs into lines preserving paragraph breaks" do
+    document = Document.new "This is an example of a long paragraph. I'm writing this specifically so that the first paragraph will need to be broken into several lines, and so that the last line will hopefully be left kind of stubby\n\nThis is the beginning of a second paragraph. I'm realizing that this test is long enough!"
+    expected = document.parse
+    expect(expected).to eq(["This is an example of a long paragraph.", "I'm writing this specifically so that", "the first paragraph will need to be", "broken into several lines, and so that", "the last line will hopefully be left", "kind of stubby", "", "This is the beginning of a second", "paragraph. I'm realizing that this test", "is long enough!"])
   end
 
 end
