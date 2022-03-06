c1695e33e63071a24e81082c7fb06f8a71da8ebb
Joseph Senecal <jasenecal2@gmail.com>
2022-03-05 17:01:02 -0700

    Feat: Document #translate

--- lib/document.rb
+++ lib/document.rb
@@ -1,14 +1,15 @@
+require './line.rb'
 class Document
 
   def initialize (input)
     @raw_text = input
-    @parsed_text = []
+    @parsed_lines = []
   end
 
   def find_breaks
     output = []
     @raw_text.each_line { |line| output << line}
-    @parsed_text = output
+    @parsed_lines = output
   end
 
   def add_breaks (line)
@@ -22,14 +23,28 @@ class Document
     output << line
   end
 
-  def parse_latin
+  def parse_lines
     find_breaks
-    @parsed_text.map! { |line| line.length > 40 ? add_breaks(line) : line }
-    @parsed_text.flatten!
-    @parsed_text.map! do |line|
+    @parsed_lines.map! { |line| line.length > 40 ? add_breaks(line) : line }
+    @parsed_lines.flatten!
+    @parsed_lines.map! do |line|
       line.chomp!
       line
     end
-    @parsed_text
+    @parsed_lines
+  end
+
+  def translate
+    parse_lines
+    # require 'pry';binding.pry
+    result = ""
+     @parsed_lines.map! {|line| Line.new(line)}
+     @parsed_lines.each do |line|
+      translated_line = line.translate
+      # require 'pry';binding.pry
+      result += translated_line
+    end
+    require 'pry'; binding.pry
+    result
   end
 end
