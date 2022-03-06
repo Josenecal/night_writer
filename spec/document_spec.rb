c269b0a8cc46b02e0a4be049221c10ea5ec04ee8
Joseph Senecal <jasenecal2@gmail.com>
2022-03-05 17:00:31 -0700

    Fix: add missing new line char to end of expected output

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -28,13 +28,13 @@ RSpec.describe Document do
 
   it "breaks down several paragraphs into lines preserving paragraph breaks" do
     document = Document.new "This is an example of a long paragraph. I'm writing this specifically so that the first paragraph will need to be broken into several lines, and so that the last line will hopefully be left kind of stubby\n\nThis is the beginning of a second paragraph. I'm realizing that this test is long enough!"
-    expected = document.parse_latin
+    expected = document.parse_lines
     expect(expected).to eq(["This is an example of a long paragraph.", "I'm writing this specifically so that", "the first paragraph will need to be", "broken into several lines, and so that", "the last line will hopefully be left", "kind of stubby", "", "This is the beginning of a second", "paragraph. I'm realizing that this test", "is long enough!"])
   end
 
   it "translates latin to braille" do
     expected = @document.translate
-    expect(expected).to eq(".oo.o...ooo..oooo...o.o.o..ooo..ooo.oo...oo.ooooo.oo..o.o.o.o....oo.o...o.o.o.oo\noooo.o..oo..o.......o.oo.ooo.o..o..o....oo....o..o.o...oo..ooo..oooo.o..o....o.o\no.......o.oo....o.....o.o..oo.....o.oo....ooo.o.......o.oo..o...o.......o...oooo\no.o.o..ooo..ooo.oo\no.oo.ooo.o...o.ooo\n..o.o..oo.....o...")
+    expect(expected).to eq(".oo.o...ooo..oooo...o.o.oo..ooo.oo...oo.ooooo.oo..o.o.o.o....oo.o...o.o.o.oo\noooo.o..oo..o.......oo.o.o..o..o....oo....o..o.o...oo..ooo..oooo.o..o....o.o\no.......o.oo....o...o.........o.oo....ooo.o.......o.oo..o...o.......o...oooo\no.o.o..ooo..ooo.oo\no.oo.ooo.o...o.ooo\n..o.o..oo.....o...\n")
   end
 
 end
