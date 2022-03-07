ed6fb657358df4e6faa3153d208daf280597ae11
Joseph Senecal <jasenecal2@gmail.com>
2022-03-07 09:59:18 -0700

    Ref: add context to tests

--- spec/rosetta_spec.rb
+++ spec/rosetta_spec.rb
@@ -6,24 +6,26 @@ SimpleCov.start
 
 RSpec.describe 'Rosetta' do
 
-  before :each do
-    @stone = Rosetta.from_nightwriter
-  end
+  context "::from_nightwriter" do
 
-  it 'initializes with no arguments or readable attributes' do
-    expect(@stone).to be_a(Rosetta)
-  end
+    before :each do
+      @stone = Rosetta.from_nightwriter
+    end
 
-  it "returns a linear braille translation of a lowercase latin letter" do
-    expect(@stone.translate("a")).to eq("o.....")
-    expect(@stone.translate("j")).to eq(".ooo..")
-  end
+    it 'initializes with no arguments or readable attributes' do
+      expect(@stone).to be_a(Rosetta)
+    end
 
-  it 'translates a line of text to three lines of braille' do
-    expected = @stone.translate_line_latin_to_braille("the quick brown fox jumped over the lazy brown dog")
-    expect(expected.length).to eq(303) #account for three new line chars
-    expect(expected[0..5]).to eq(".oo.o.") # top dots of "the"
-    expect(expected[-7..-1]).to eq("..o...\n")# bottom dots of "dog"
-  end
+    it "returns a linear braille translation of a lowercase latin letter" do
+      expect(@stone.translate("a")).to eq("o.....")
+      expect(@stone.translate("j")).to eq(".ooo..")
+    end
 
+    it 'translates a line of text to three lines of braille' do
+      expected = @stone.translate_line_latin_to_braille("the quick brown fox jumped over the lazy brown dog")
+      expect(expected.length).to eq(303) #account for three new line chars
+      expect(expected[0..5]).to eq(".oo.o.") # top dots of "the"
+      expect(expected[-7..-1]).to eq("..o...\n")# bottom dots of "dog"
+    end
+  end
 end
