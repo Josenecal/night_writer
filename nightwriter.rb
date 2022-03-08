dce778006e32d4b6f84afd5f0cca59a8a6849539
Joseph Senecal <jasenecal2@gmail.com>
2022-03-07 22:00:33 -0700

    Feat: implement new document counters in output text

--- nightwriter.rb
+++ nightwriter.rb
@@ -21,7 +21,6 @@ document = LatinDocument.new(input.read)
 translated_text = document.get_translation
 output = File.open(ARGV[1], "w")
 output.write(translated_text)
-text_length = input.read.length
-puts "Created '#{ARGV[1]}' containing #{text_length} characters"
+puts "Created '#{ARGV[1]}' containing #{document.length} characters"
 input.close
 output.close
